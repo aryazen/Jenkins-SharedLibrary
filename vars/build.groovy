524f59ee1355aee4de809b92964be73a6710ea34
aryazen <69396823+aryazen@users.noreply.github.com>
2024-04-24 21:06:18 +0000

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,19 +1,20 @@
 def java(){
     echo "Building java project"
     sh '''
-      mvn clean package
+      npm install 
+      npm test
     '''
 }
 
-def react(){
-  echo "Building React Application"
-  bat """
-    cd /d F:
-    cd F:\\${appName}\\
-    npm run build
-  """
+// def react(){
+//   echo "Building React Application"
+//   bat """
+//     cd /d F:
+//     cd F:\\${appName}\\
+//     npm run build
+//   """
 
-    bat """
-        powershell Compress-Archive -Path "F:\\${appName}\\build\\*" -DestinationPath "F:\\${appName}_${BUILD_NUMBER}.zip"
-    """
-}
+//     bat """
+//         powershell Compress-Archive -Path "F:\\${appName}\\build\\*" -DestinationPath "F:\\${appName}_${BUILD_NUMBER}.zip"
+//     """
+// }
